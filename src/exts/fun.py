80dc426b90e34978d9ba36db43f9ea765876a8b1
VaibhavSys <vaibhavsys@protonmail.com>
2024-06-25 22:32:42 +0530

    feat: react with egg on andrew message

---
+++ src/exts/fun.py
@@ -0,0 +1,17 @@
+import nextcord
+from nextcord.ext import commands
+from bot import Bot
+
+
+class Utils(commands.Cog):
+    def __init__(self, bot: Bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message: nextcord.Message):
+        if message.author.id in [598245488977903688]:
+            message.add_reaction("🥚")
+
+
+def setup(bot: Bot):
+    bot.add_cog(Utils(bot))
