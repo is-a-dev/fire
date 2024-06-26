c6fc42b387d6e5d1bf20d6ba98ed02ad507eb48d
VaibhavSys <vaibhavsys@protonmail.com>
2024-06-26 20:35:05 +0530

    chore: remove add reactions on msg

--- src/exts/fun.py
+++ src/exts/fun.py
@@ -8,13 +8,6 @@ class Utils(commands.Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
 
-    @commands.Cog.listener()
-    async def on_message(self, message: nextcord.Message):
-        if message.author.id in [598245488977903688] and "." in message.content:
-            await message.add_reaction("🥚")
-        if message.author.id in [961063229168164864] and "." in message.content:
-            await message.add_reaction("🐒")
-
     @nextcord.slash_command()
     async def toss(self, interaction: nextcord.Interaction):
         choice = random.choice(["Heads", "Tails"])
